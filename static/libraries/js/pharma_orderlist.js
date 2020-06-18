494565dc78e6ae17ba0edfb4b8dcc39d8811c6b0
keung <38097602+bluegmlduf2@users.noreply.github.com>
2020-06-19 00:57:01 +0900

    20200619

--- static/libraries/js/pharma_orderlist.js
+++ static/libraries/js/pharma_orderlist.js
@@ -59,172 +59,169 @@ $(function () {
 
 	$('#prodList').prepend(addRow);
 
+    
 	//Save Order
 	$('#btnSaveOrder').click(function () {
-		swal({
-				title: "Would you Save Order?",
-				text: "Check Your Order Number! \n     [ " + $('#ORDER_CD').val() + " ] ",
-				icon: "info",
-				confirmButtonColor: '#3085d6',
-				cancelButtonColor: '#d33',
-				buttons: true
-			})
-			.then((willDelete) => {
-                var obj = {
-                    "ORDER_CD":$('#ORDER_CD').val(),
-                    "ORDER_NATION":$('#ORDER_NATION').val(),
-                    "ORDER_NM":$('#ORDER_NM').val(),
-                    "ORDER_CONTRY":$('#ORDER_CONTRY').val(),
-                    "ORDER_COMPANY":$('#ORDER_COMPANY').val(),
-                    "ORDER_ADDR":$('#ORDER_ADDR').val(),
-                    "ORDER_POST":$('#ORDER_POST').val(),
-                    "ORDER_EMAIL":$('#ORDER_EMAIL').val(),
-                    "ORDER_PHONE":$('#ORDER_PHONE').val(),
-                    "ORDER_WANT":$('#ORDER_WANT').val()
-                };
+        var obj = {
+            "ORDER_CD": $('#ORDER_CD').val(),
+            "ORDER_NATION": $('#ORDER_NATION').val(),
+            "ORDER_NM": $('#ORDER_NM').val(),
+            "ORDER_CONTRY": $('#ORDER_CONTRY').val(),
+            "ORDER_COMPANY": $('#ORDER_COMPANY').val(),
+            "ORDER_ADDR": $('#ORDER_ADDR').val(),
+            "ORDER_POST": $('#ORDER_POST').val(),
+            "ORDER_EMAIL": $('#ORDER_EMAIL').val(),
+            "ORDER_PHONE": $('#ORDER_PHONE').val(),
+            "ORDER_WANT": $('#ORDER_WANT').val()
+        };
 
-                obj = JSON.stringify(obj); //json객체 -> json문자열
-debugger
-				if (validationChk(obj)) {
-                    alert('1111true');
-                    // if (willDelete) {
-					// 	$.ajax({
-					// 		type: "POST",
-					// 		url: "/pharmaShop/main/updateOrderList",
-					// 		data: {
-					// 			"data": obj
-					// 		},
-					// 		async: false,
-					// 		dataType: "json",
-					// 		success: function (result) {
-					// 			// if (result.order_cd[0].CNT == 1) {
-					// 			// 	swal("Thanks!", "Successfully Checked!", "success");
-					// 			// 	var rUrl = "/pharmaShop/main/orderList/" + value;
-					// 			// 	location.href = rUrl;
-					// 			// } else {
-					// 			// 	swal("Sorry Check the OrderNumber!", result.message, "error");
-                    //             // }
-                                
+        obj = JSON.stringify(obj); //json객체 -> json문자열
 
+		if (validationChk(obj)) {
+			swal({
+					title: "Would you Save Order?",
+					text: "Check Your Order Number! \n 　　　　　[ " + $('#ORDER_CD').val() + " ] ",
+					icon: "info",
+					confirmButtonColor: '#3085d6',
+					cancelButtonColor: '#d33',
+					buttons: true
+				})
+				.then((willDelete) => {
+					if (willDelete) {
+						// if (willDelete) {
+						// 	$.ajax({
+						// 		type: "POST",
+						// 		url: "/pharmaShop/main/updateOrderList",
+						// 		data: {
+						// 			"data": obj
+						// 		},
+						// 		async: false,
+						// 		dataType: "json",
+						// 		success: function (result) {
+						// 			// if (result.order_cd[0].CNT == 1) {
+						// 			// 	swal("Thanks!", "Successfully Checked!", "success");
+						// 			// 	var rUrl = "/pharmaShop/main/orderList/" + value;
+						// 			// 	location.href = rUrl;
+						// 			// } else {
+						// 			// 	swal("Sorry Check the OrderNumber!", result.message, "error");
+						//             // }
 
 
-					// 			//콜백함수-->동기적으로 동작 location.reload();가 비동기적으로 실행되버리기때문
-                    //             /*
-                    //             var syncFunc=function(lastCall){
 
-					// 			주문번호 
-					// 			swal("Successfully Ordered!", "Check Your Order Number!  \n Youu OrderNumber Is : ", "success");
 
-					// 			//로컬스토리지 비우기 확인
-					// 			swal({
-					// 			    title: "Successfully Ordered!",
-					// 			    text: "Check Your Order Number! [ "+result.result+" ]  \n  Would you delete Cart List?",
-					// 			    icon: "success",
-					// 			    confirmButtonColor: '#3085d6',
-					// 			    cancelButtonColor: '#d33',
-					// 			    buttons: true
-					// 			})
-					// 			.then((willDelete) => {
-					// 			    if (willDelete) {
-					// 			        localStorage.clear();
-					// 			        swal("Successfully Deleted");
-					// 			        location.reload();
-					// 			    }else{
-					// 			        location.reload();
-					// 			    }
-					// 			});  
-					// 			//최종 실행 location.reload();
-					// 			lastCall();
-					// 			}  */                               
-					// 		},
-					// 		error: function (request, status, error) {
-					// 			//console.log("code:"+request.status+ ", message: "+request.responseText+", error:"+error);
-					// 			swal("code:" + request.status + ", message: " + request.responseText + ", error:" +
-					// 				error + "\n" + "\n     ---Please Contact Administrator ---");
-					// 		}
-					// 	});
-					// }
-				}else{
-                    alert('???false');
-                }
-			});
+						// 			//콜백함수-->동기적으로 동작 location.reload();가 비동기적으로 실행되버리기때문
+						//             /*
+						//             var syncFunc=function(lastCall){
+
+						// 			주문번호 
+						// 			swal("Successfully Ordered!", "Check Your Order Number!  \n Youu OrderNumber Is : ", "success");
+
+						// 			//로컬스토리지 비우기 확인
+						// 			swal({
+						// 			    title: "Successfully Ordered!",
+						// 			    text: "Check Your Order Number! [ "+result.result+" ]  \n  Would you delete Cart List?",
+						// 			    icon: "success",
+						// 			    confirmButtonColor: '#3085d6',
+						// 			    cancelButtonColor: '#d33',
+						// 			    buttons: true
+						// 			})
+						// 			.then((willDelete) => {
+						// 			    if (willDelete) {
+						// 			        localStorage.clear();
+						// 			        swal("Successfully Deleted");
+						// 			        location.reload();
+						// 			    }else{
+						// 			        location.reload();
+						// 			    }
+						// 			});  
+						// 			//최종 실행 location.reload();
+						// 			lastCall();
+						// 			}  */                               
+						// 		},
+						// 		error: function (request, status, error) {
+						// 			//console.log("code:"+request.status+ ", message: "+request.responseText+", error:"+error);
+						// 			swal("code:" + request.status + ", message: " + request.responseText + ", error:" +
+						// 				error + "\n" + "\n     ---Please Contact Administrator ---");
+						// 		}
+						// 	});
+						// }
+					}
+				});
+		}
 	});
 
 	//Cancel Order
 	$('#btnCancelOrder').click(function () {
-        $('#ORDER_CD').val('');
-        $('#ORDER_NATION').val('');
-        $('#ORDER_NM').val('');
-        $('#ORDER_CONTRY').val('');
-        $('#ORDER_COMPANY').val('');
-        $('#ORDER_ADDR').val('');
-        $('#ORDER_POST').val('');
-        $('#ORDER_EMAIL').val('');
-        $('#ORDER_PHONE').val('');
-        $('#COUPON_CD').val('');
-        $('#ORDER_AMOUNT').val('');
-        $('#ORDER_DATE').val('');
-        $('#ORDER_WANT').val('');
-    });
-    
-    //validation Check
-    function validationChk(obj) {
-        var chk = true;
-        var output = "";
-        // var obj = {
-        //     "ORDER_CD":$('#ORDER_CD').val(),
-        //     "ORDER_NATION":$('#ORDER_NATION').val(),
-        //     "ORDER_NM":$('#ORDER_NM').val(),
-        //     "ORDER_CONTRY":$('#ORDER_CONTRY').val(),
-        //     "ORDER_COMPANY":$('#ORDER_COMPANY').val(),
-        //     "ORDER_ADDR":$('#ORDER_ADDR').val(),
-        //     "ORDER_POST":$('#ORDER_POST').val(),
-        //     "ORDER_EMAIL":$('#ORDER_EMAIL').val(),
-        //     "ORDER_PHONE":$('#ORDER_PHONE').val()
-        // };
+		$('#ORDER_NATION').val('1');
+		$('#ORDER_NM').val('');
+		$('#ORDER_CONTRY').val('');
+		$('#ORDER_COMPANY').val('');
+		$('#ORDER_ADDR').val('');
+		$('#ORDER_POST').val('');
+		$('#ORDER_EMAIL').val('');
+		$('#ORDER_PHONE').val('');
+		$('#ORDER_WANT').val('');
+	});
 
-        var arrCol=[
-            '[ Name ]\n',
-            '[ State / Country ]\n',          
-            '[ Address ]\n',            
-            '[ Posta / Zip ]\n',
-            '[ Email Address ]\n',
-            '[ Phone ]\n'
-        ];
+	//validation Check
+	function validationChk(obj) {
+		var chk = true;
+		var output = "";
+		// var obj = {
+		//     "ORDER_CD":$('#ORDER_CD').val(),
+		//     "ORDER_NATION":$('#ORDER_NATION').val(),
+		//     "ORDER_NM":$('#ORDER_NM').val(),
+		//     "ORDER_CONTRY":$('#ORDER_CONTRY').val(),
+		//     "ORDER_COMPANY":$('#ORDER_COMPANY').val(),
+		//     "ORDER_ADDR":$('#ORDER_ADDR').val(),
+		//     "ORDER_POST":$('#ORDER_POST').val(),
+		//     "ORDER_EMAIL":$('#ORDER_EMAIL').val(),
+		//     "ORDER_PHONE":$('#ORDER_PHONE').val()
+		// };
 
-        var contact = JSON.parse(obj);//json문자열 ->js객체
-        var i=0;
+		//obj에 있는 값 전부다 입력해줘야함
+		var arrCol = [
+			'[ OrderCode ]\n',
+			'[ Country ]\n',
+			'[ Name ]\n',
+			'[ State / Country ]\n',
+			'[ Company ]\n',
+			'[ Address ]\n',
+			'[ Posta / Zip ]\n',
+			'[ Email Address ]\n',
+			'[ Phone ]\n',
+			'[ Message ]\n'
+		];
 
-        //국가선택체크
-        if(contact.ORDER_NATION==1){
-            output+="[ Country ]\n";
-            chk = false;
-        }
+		var contact = JSON.parse(obj); //json문자열 ->js객체
+		var i = 0;
 
-        //공백확인
-        $.each(contact, function (index, item) {
+		//국가선택체크
+		if (contact.ORDER_NATION == 1) {
+			output += "[ Country ]\n";
+			chk = false;
+		}
 
-            if(index!="ORDER_NATION"
-            &&index!="ORDER_NM"
-            &&index!="ORDER_CONTRY"
-            &&index!="ORDER_ADDR"
-            &&index!="ORDER_POST"
-            &&index!="ORDER_EMAIL"
-            &&index!="ORDER_PHONE"){
-                if (item == "") {
-                    //$("#idNm").focus(); 
-                    output += arrCol[i];
-                    chk = false;
-                }     
-            }
-            i++
-        });
+		//공백확인
+		$.each(contact, function (index, item) {
+			//if()검사하지않는 항목
+			if (index != "ORDER_CD" &&
+				index != "ORDER_NATION" &&
+				index != "ORDER_COMPANY" &&
+				index != "ORDER_WANT") {
+				if (item == "") {
+					output += arrCol[i];
+					chk = false;
+				}
+			}
+			i++
+		});
 
-        //에러메세지 출력
-        if (output != "") {
-            swal("** Please Check input Value **", output);
-        }
+		//에러메세지 출력
+		if (output != "") {
+			swal("** Please Check input Value **", output);
+		}
 
-        return chk;
-    }
+		return chk;
+	}
 });
